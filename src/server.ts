d175d20a9b7e898e91474111c82258379e257c78
Chris Poulter <cpoulter@hotmail.co.uk>
2026-04-03 14:31:25 +0100

    refactor: move app logic to app.ts and update server initialization

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
-import app from "./index.js";
+import { createApp } from "./app.js";
 import { env } from "./env.js";
+
+const app = createApp();
 
 app.listen(env.PORT, () => {
   console.log(`Server is running on http://localhost:${env.PORT}`);

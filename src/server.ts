d4c9287059fa374225e3c726f9b774444a2d8d75
Chris Poulter <cpoulter@hotmail.co.uk>
2026-04-27 11:55:28 +0100

    refactor: restructure app initialization by moving to index.ts and removing createApp function

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,6 @@
-import { createApp } from "./app.js";
+import app from "./index.js";
 import { config } from "./lib/config.js";
 import { logger } from "./lib/logger.js";
-
-const app = createApp();
 
 app.listen(config.PORT, () => {
   logger.info({ port: config.PORT }, "Server started");

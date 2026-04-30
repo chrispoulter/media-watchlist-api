9761735699e38dcc51a4b78070f59e0408e51fde
Chris Poulter <cpoulter@hotmail.co.uk>
2026-04-30 16:55:13 +0100

    refactor: remove Sentry initialization logging

--- src/lib/instrument.ts
+++ src/lib/instrument.ts
@@ -1,17 +1,7 @@
 import * as Sentry from '@sentry/node';
 import { config, release, environment } from './config.js';
-import { logger } from './logger.js';
 
 if (config.SENTRY_DSN) {
-    logger.info(
-        {
-            dsn: config.SENTRY_DSN,
-            release,
-            environment,
-        },
-        'Initializing Sentry'
-    );
-
     Sentry.init({
         dsn: config.SENTRY_DSN,
         release,

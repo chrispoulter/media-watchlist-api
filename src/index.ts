8d574d8f5608bc3d9f6cafc82a509fdd94ba03ff
Chris Poulter <cpoulter@hotmail.co.uk>
2026-05-07 09:27:18 +0100

    fix: remove duplicate request logger middleware usage in index file

--- src/index.ts
+++ src/index.ts
@@ -26,10 +26,9 @@ app.use(
     })
 );
 
+app.use(requestLogger);
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
-
-app.use(requestLogger);
 
 app.all('/api/auth/*splat', toNodeHandler(auth));
 app.use('/api/search', searchRouter);

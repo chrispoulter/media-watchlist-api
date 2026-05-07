b18025e87d26c0dbe31b53bf4872ad43d6a375bf
Chris Poulter <cpoulter@hotmail.co.uk>
2026-05-07 17:09:04 +0100

    refactor: simplify home route by removing unused schema validation

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -1,22 +1,9 @@
 import { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
-import z from 'zod';
 
 const plugin: FastifyPluginAsyncZod = async (fastify) => {
-    fastify.get(
-        '/',
-        {
-            schema: {
-                response: {
-                    200: z.object({
-                        message: z.string(),
-                    }),
-                },
-            },
-        },
-        async function () {
-            return { message: 'Welcome to the Media Watchlist API!' };
-        }
-    );
+    fastify.get('/', async function () {
+        return { message: 'Welcome to the Media Watchlist API!' };
+    });
 };
 
 export default plugin;

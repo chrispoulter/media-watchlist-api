90bf18840f4424409db013d9411e2c5c5ad46ec4
Chris Poulter <cpoulter@hotmail.co.uk>
2026-06-18 15:19:13 +0100

    feat: add user, session, account, two_factor, verification, and watchlist_item tables with relationships and indexes (#12)

--- drizzle/0000_bizarre_mattie_franklin.sql
+++ drizzle/0000_bizarre_mattie_franklin.sql
@@ -30,7 +30,8 @@ CREATE TABLE "two_factor" (
 	"id" text PRIMARY KEY NOT NULL,
 	"secret" text NOT NULL,
 	"backup_codes" text NOT NULL,
-	"user_id" text NOT NULL
+	"user_id" text NOT NULL,
+	"verified" boolean DEFAULT true
 );
 --> statement-breakpoint
 CREATE TABLE "user" (
